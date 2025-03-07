a7c86ab9fa87c817189d9563b9861a6d82fa708c
Yahir Valero <l21070330@cdmadero.tecnmn.mx>
2025-03-07 11:51:01 -0600

    Agregué un comentario

--- GreetingCard2/app/src/main/java/com/example/greetingcard2/MainActivity.kt
+++ GreetingCard2/app/src/main/java/com/example/greetingcard2/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.example.greetingcard2
 
-import android.os.Bundle
+import android.os.Bundle //Comienzo de importaciones
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
