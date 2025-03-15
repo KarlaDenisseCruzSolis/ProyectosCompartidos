1f3caeab0c06501ff9b2ced5443a08448f6c17e2
KarlaCruz <karladenisse684@gmail.com>
2025-03-15 17:07:10 -0600

    Comentarios en Ejemplo2

--- 20_EjemplosApp/Ejemplo2/app/src/main/java/com/example/ejemplo2/MainActivity.kt
+++ 20_EjemplosApp/Ejemplo2/app/src/main/java/com/example/ejemplo2/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.ejemplo2
+// Paquete que contiene la clase MainActivity
 
 import android.graphics.Color
 import android.os.Bundle
@@ -6,15 +7,16 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import java.util.Random
+// Importación de las clases necesarias para la funcionalidad de la aplicación
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : AppCompatActivity() { // Define la clase MainActivity que hereda de AppCompatActivity
+    override fun onCreate(savedInstanceState: Bundle?) {  // Metodo que se ejecuta cuando la actividad se crea por primera vez
+        super.onCreate(savedInstanceState) // Llama al metodo de la clase padre para inicializar la actividad
+        setContentView(R.layout.activity_main) // Asocia el archivo de diseño activity_main.xml con esta actividad
 
         // Obtener referencias al diseño y al botón
-        val mainLayout: ConstraintLayout = findViewById(R.id.mainLayout)
-        val changeColorButton: Button = findViewById(R.id.changeColorButton)
+        val mainLayout: ConstraintLayout = findViewById(R.id.mainLayout) // Asocia el ConstraintLayout con el ID definido en el archivo XML
+        val changeColorButton: Button = findViewById(R.id.changeColorButton) // Asocia el Button con el ID definido en el archivo XML
 
         // Establecer un listener de clic para el botón
         changeColorButton.setOnClickListener {
@@ -28,7 +30,7 @@ class MainActivity : AppCompatActivity() {
 
     // Función para generar un color aleatorio
     private fun getRandomColor(): Int {
-        val random = Random()
-        return Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256))
+        val random = Random() // Crea una nueva instancia de la clase Random
+        return Color.argb(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)) // Canal alfa (opacidad) completamente visible, Valor de rojo entre 0 y 255, Valor de verde entre 0 y 255,Valor de azul entre 0 y 255.
     }
 }
