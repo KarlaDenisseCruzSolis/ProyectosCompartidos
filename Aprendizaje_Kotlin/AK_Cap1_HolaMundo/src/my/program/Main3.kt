91300c53b2c8412af5d33b5f2b94a093c63fc6f8
KarlaCruz <karladenisse684@gmail.com>
2025-03-19 09:17:57 -0600

    Agregando Cap1 HolaMundo Aprendizaje_Kotlin

---
+++ Aprendizaje_Kotlin/AK_Cap1_HolaMundo/src/my/program/Main3.kt
@@ -0,0 +1,13 @@
+//Hola Mundo usando un objeto compañero
+package my.program
+
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World")
+        }
+    }
+}
+
+
